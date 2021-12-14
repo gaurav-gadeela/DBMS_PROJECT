fac5e94c0997344531e4ebc446154148d671ebda
gaurav-gadeela <88039739+gaurav-gadeela@users.noreply.github.com>
2021-12-14 23:41:12 +0530

    Add files via upload

---
+++ search.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     <style>
+         #maincontainer{
+             min-height:433px;
+         }
+         </style>
+
+    
+    <title>Hello, world!</title>
+</head>
+
+<body>
+<?php include 'partials/dbconnect.php';?>
+<?php include 'partials/header.php';?>
+
+
+<div class="container my-3" id="maincontainer">
+        <h1 class="py-3">Search results for <em>"<?php echo $_GET['search']?>"</em></h1>
+
+        <?php  
+        $noresults = true;
+        $query = $_GET["search"];
+        $sql = "SELECT * FROM `threads` WHERE MATCH (`thread_title`, `thread_desc`) AGAINST (`$query`)"; 
+        $result = mysqli_query($conn, $sql);
+        while($row = mysqli_fetch_assoc($result)){
+            $title = $row['thread_title'];
+            $desc = $row['thread_desc']; 
+            $thread_id= $row['thread_id'];
+            $url = "thread.php?threadid=". $thread_id;
+            $noresults = false;
+
+            // Display the search result
+            echo '<div >
+                        <h3><a href="'. $url. '" class="text-dark">'. $title. '</a> </h3>
+                        <p>'. $desc .'</p>
+                  </div>'; 
+            }
+        if ($noresults){
+            echo '<div class="container container-fluid">
+                    <div class="container">
+                        <p class="display-4">No Results Found</p>
+                        <p > Suggestions: <ul>
+                                <li>Make sure that all words are spelled correctly.</li>
+                                <li>Try different keywords.</li>
+                                <li>Try more general keywords. </li></ul>
+                        </p>
+                    </div>
+                 </div>';
+        }        
+    ?>
+
+
+  
+  </div>
+
+    <?php include 'partials/footer.php';?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
